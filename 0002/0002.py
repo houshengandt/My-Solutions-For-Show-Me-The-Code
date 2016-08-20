da9daae3ad7931765193909f050f2377a3fcbc67
houshengandt <houshengandt@gmail.com>
2016-08-20 21:34:51 +0800

    0002 completed

---
+++ 0002/0002.py
@@ -0,0 +1,11 @@
+import MySQLdb
+
+db = MySQLdb.connect("localhost", "root", passwd="******", db="u0002")
+# 填写数据库信息
+
+cursor = db.cursor()
+cursor.execute("DROP TABLE IF EXISTS U0002")    # will be a warning! :Warning: Unknown table 'u0002.u0002'
+cursor.execute("CREATE TABLE U0002 (mykey VARCHAR(36))")
+cursor.execute("LOAD DATA LOCAL INFILE 'key.txt' INTO TABLE u0002 LINES TERMINATED BY '\r\n';")
+db.commit()
+db.close()
